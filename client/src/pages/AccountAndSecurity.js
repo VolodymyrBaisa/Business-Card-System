28048faef0d19652ffe1bcfe02e82a9b8e7d0b42
Vllaadd <vlladdiimiir@gmail.com>
2021-01-13 03:28:17 -0500

    UploadNewCard component created

--- client/src/pages/AccountAndSecurity.js
+++ client/src/pages/AccountAndSecurity.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { CreateAnAccount } from '../components/CreateAnAccount'
+import { CreateAnAccount } from '../components/CreateAnAccount';
 
 export default class AccountAndSecurity extends Component {
     render() {
--- client/src/pages/CardUpload.js
+++ client/src/pages/CardUpload.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { Header } from '../components/Header';
+import { UploadNewCard } from '../components/UploadNewCard';
 
 export default class CardUpload extends Component {
     render() {
-        return 
-        <>
-            <Header />
-        </>;
+        return(
+            <UploadNewCard />
+        )
+        
     }
 }
