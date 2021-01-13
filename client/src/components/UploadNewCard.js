a04bd6da0b12614bca88e88eac99f4664feb8ddb
Vllaadd <vlladdiimiir@gmail.com>
2021-01-13 03:36:16 -0500

    fixed closing tag typo

--- client/src/components/UploadNewCard.js
+++ client/src/components/UploadNewCard.js
@@ -1,37 +1,36 @@
 import React from "react";
-import { Header } from "./Header";
+import  Header  from "./Header";
 
 export const CreateAnAccount = () => {
     return (
         <>
-        <Header />
-        <div className="container">
-    
-        <div className="upload"></div>
-        <form action="">
-            <div className="cardtag">
-                <label for="cardtag">Card Tag:</label>
-                <input type="text" name="cardtag" />
-                <label for="cardtags">Card Tags:</label>
-                <input type="text" name="cardtags" />
+            <Header />
+            <div className="container">
+                <div className="upload"></div>
+                <form action="">
+                    <div className="cardtag">
+                        <label for="cardtag">Card Tag:</label>
+                        <input type="text" name="cardtag" />
+                        <label for="cardtags">Card Tags:</label>
+                        <input type="text" name="cardtags" />
+                    </div>
+                    <div className="name">
+                        <label for="firstname">First Name:</label>
+                        <input type="text" name="firstname" />
+                        <label for="lastname">Last Name:</label>
+                        <input type="text" name="lastname" />
+                    </div>
+                    <label for="companyname">Company Name:</label>
+                    <input type="text" name="companyname" />
+                    <label for="phonenumber">Phone Number:</label>
+                    <input type="text" name="phonenumber" />
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" />
+                    <label for="address">Address:</label>
+                    <input type="text" name="address" />
+                    <input type="submit" value="Save Card" />
+                </form>
             </div>
-            <div className="name">
-                <label for="firstname">First Name:</label>
-                <input type="text" name="firstname" />
-                <label for="lastname">Last Name:</label>
-                <input type="text" name="lastname" />
-            </div>
-            <label for="companyname">Company Name:</label>
-            <input type="text" name="companyname" />
-            <label for="phonenumber">Phone Number:</label>
-            <input type="text" name="phonenumber" />
-            <label for="email">Email:</label>
-            <input type="email" name="email" />
-            <label for="address">Address:</label>
-            <input type="text" name="address" />
-            <input type="submit" value="Save Card">
-        </form>
-    </div>
     </>
-)
+    )
 }
