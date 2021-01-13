8340ca9935af5b7473b3c11f959b37ac15643e19
Vllaadd <vlladdiimiir@gmail.com>
2021-01-13 02:09:47 -0500

    CreateAnAccount styling

--- client/src/components/CreateAnAccount.js
+++ client/src/components/CreateAnAccount.js
@@ -6,17 +6,20 @@ export const CreateAnAccount = () => {
         <>
             <Header />
             <div className="credentials">
-                <label for="first_name">First Name:</label>
-                <input type="text" name="first_name" />
-                <label for="last_name">Last Name:</label>
-                <input type="text" name="last_name" /><br/>
-                <label for="email">Email:</label>
-                <input type="email" name="email" /><br/>
-                <label for="password">New Password:</label>
-                <input type="password" name="password" />
-                <label for="confirm_password">Confirm Password:</label>
-                <input type="password" name="confirm_password" /><br/>
-                <button className="save-button">Save Profile</button>
+                <img src="/img/user.htm" alt="user image"></img>
+                    <div className="forms">
+                        <label for="first_name">First Name:</label>
+                        <input type="text" name="first_name" />
+                        <label for="last_name">Last Name:</label>
+                        <input type="text" name="last_name" /><br />
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" /><br />
+                        <label for="password">New Password:</label>
+                        <input type="password" name="password" />
+                        <label for="confirm_password">Confirm Password:</label>
+                        <input type="password" name="confirm_password" /><br />
+                        <button className="save-button">Save Profile</button>
+                    </div>
             </div>
         </>
     )
