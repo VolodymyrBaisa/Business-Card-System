14a1f956691a550319e52a2a3b05e86b6d48a0b7
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-13 05:55:28 -0500

    db name = dbc_db

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbc-system", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbc_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
