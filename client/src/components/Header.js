15dc7b79042db6c3ff670384598d2efac4ed1938
mini pony <northjw1@gmail.com>
2021-01-13 21:05:35 -0500

    setting up authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 import TopGlassCover from "../img/svg/top-glass-cover.svg";
 
 export const Header = () => {
+    useLogRender();
     return (
         <div className="top-container">
             <div className="top-glass-cover">
