7a67820b0b0fe393e0e7bca113c4ff429f3c363b
Volodymyr Baisa <biosvova@gmail.com>
2021-01-16 16:12:13 -0500

    add clieant side

--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -1,43 +1,30 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 
-const userController = require("../../../controllers/userController");
-// Requiring passport
-const passport = require("../../utils/passport");
+// const userController = require("../../../controllers/userController");
+// // Requiring passport
+// const passport = require("../../utils/passport");
 
-// Matches with "/api/user/login"
-router
-  .route("/login")
-  // passport.authenticate middleware
-  // If the user has valid login credentials go to the members page.
-  // Otherwise the user will be sent an error
-  .post(passport.authenticate("local"), userController.login);
+// // Matches with "/api/user/login"
+// router
+//     .route("/login")
+//     // passport.authenticate middleware
+//     // If the user has valid login credentials go to the members page.
+//     // Otherwise the user will be sent an error
+//     .post(passport.authenticate("local"), userController.login);
 
-// Matches with "/api/user/signup"
-router.route("/signup").post(userController.signup);
+// // Matches with "/api/user/signup"
+// router.route("/signup").post(userController.signup);
 
-// Matches with "/api/user/logout"
-router
-  .route("/logout")
-  // Route for logging user out
-  .get(userController.logout);
+// // Matches with "/api/user/logout"
+// router
+//     .route("/logout")
+//     // Route for logging user out
+//     .get(userController.logout);
 
-// Matches with "/api/user/authenticate"
-router
-  .route("/authenticate")
-  // Route for getting user data
-  .post(userController.authenticate);
-
-module.exports = router;
-=======
-const router = require("express").Router();
-const commentRoutes = require("./comments");
-const userRoutes = require("./user");
-
-// comments routes
-router.use("/comments", commentRoutes);
-// user routes
-router.use("/user", userRoutes);
+// // Matches with "/api/user/authenticate"
+// router
+//     .route("/authenticate")
+//     // Route for getting user data
+//     .post(userController.authenticate);
 
 module.exports = router;
->>>>>>> main
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -11,38 +10,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+    app.use(express.static("../client/build"));
 }
 app.use(routes);
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbc_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-app.listen(PORT, () => {
-  console.log(`API Server now listening on PORT ${PORT}!`);
-});
-=======
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
-}
-app.use(routes);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbc-system", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -52,4 +25,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbc-system", {
 app.listen(PORT, () => {
     console.log(`API Server now listening on PORT ${PORT}!`);
 });
->>>>>>> main
